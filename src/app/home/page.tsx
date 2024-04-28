1bb0e02b121cf8e0f59d73438343db936e6487d1
Yeminem <omdnrd@gmail.com>
2024-04-28 11:56:59 -0500

    final final commit

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -6,13 +6,13 @@ import {Accordion, AccordionItem} from "@nextui-org/react";
 export default function Home() {
   return (   
     <div className="flex gap-4 items-center">
-    <b>Current Balance $0.00</b>
+    <b>Current Balance $750.22</b>
     <br></br>
       <Button size="lg" style={{display: "inline-block", marginRight: 20}}>
-        Borrowed $0.00
+        Borrowed $2,200.00
       </Button>  
       <Button size="lg" style={{display: "inline-block", marginRight: 20}}>
-        Loaned $0.00
+        Loaned $400.00
       </Button>  
       <Button size="lg" style={{display: "inline-block", marginRight: 20}}>
         Loans 
