fbb08a2e40b5e34eac75296d396ae3c6de018fcc
Yeminem <omdnrd@gmail.com>
2024-04-28 11:53:00 -0500

    final commit

--- src/app/paymentconfirmation/page.tsx
+++ src/app/paymentconfirmation/page.tsx
@@ -1,35 +1,45 @@
+"use client"
 
 import React from "react";
 import {Input, DatePicker } from "@nextui-org/react";
 import {Button, ButtonGroup} from "@nextui-org/react";
 import {Card, CardBody} from "@nextui-org/react";
+import { useRouter } from "next/navigation";
 
 
 export default function paymentconfirmation() {
+  const router = useRouter()
   return (
     <Card>
+     <div className="flex w-full flex-wrap md:flex-nowrap gap-4 max-w-[500px]" style={{justifyContent: "center"}}>
+
       <CardBody>
-        <p>Loan Amount: $0.00</p>
+        <p>Loan Amount: $500</p>
           <br></br>
-        <p>Details about Loan:</p>
+        <p>Interest: 2%</p>
+          <br></br>
+        <p>Loan Term: 6 months</p>
           <br></br>
         <p>Make a Payment:</p>
           <br></br>
         <p></p>
-
-        <Button color="primary">
-          Full Payment $0.00
+        <Button color="primary" size="lg" onClick={() => router.push('/home')}>
+          Full Payment $502.92
         </Button> 
-        <Button color="primary">
-          Minimum Payment $0.00
+        <br></br>
+        <Button color="primary" size="lg" >
+          Minimum Payment $83.92
         </Button>      
-        <Button color="primary">
+        <br></br>
+        <Button color="primary" size="lg" >
           Custom Payment
         </Button> 
-        <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
+        <div className="flex w-full flex-wrap md:flex-nowrap gap-4 max-w-[500px]" >
           <Input type="input" label="Payment amount" />
         </div>
       </CardBody>
+      </div>
+
     </Card>
   );
 }
