c69c4f138a0c31e6dc88a51ac06c77da14898a2c
Yeminem <omdnrd@gmail.com>
2024-04-27 22:42:32 -0500

    Initial Commit

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,24 @@
+import React from "react";
+import {Button, ButtonGroup} from "@nextui-org/react";
+import {Divider} from "@nextui-org/react";
+import {Accordion, AccordionItem} from "@nextui-org/react";
 
 export default function Home() {
   return (
-  <p>home</p>
+   
+    <div className="flex gap-4 items-center">
+      <Button size="lg">
+        Borrowed $
+      </Button>  
+      <Button size="lg">
+        Loaned $
+      </Button>  
+      <Button size="lg">
+        Loans
+      </Button>  
+      <Button size="lg">
+        Get Loans
+      </Button> 
+    </div>    
   );
 }
--- src/app/loan/page.tsx
+++ src/app/loan/page.tsx
@@ -1,6 +1,79 @@
+import React from "react";
+import {Input} from "@nextui-org/react";
+import {DatePicker} from "@nextui-org/react";
+import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell} from "@nextui-org/react";
 
-export default function loan() {
+
+export default function App() {
+  const placements = [
+    "outside",
+  ];
   return (
-  <p>loan</p>
+    <div className="flex flex-col gap-4">
+      <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
+        <Input
+          label="Interest"
+          placeholder="0.00%"
+          labelPlacement="outside"
+        />
+        <Input
+          label="Amount"
+          placeholder="0.00"
+          labelPlacement="outside"
+          startContent={
+            <div className="pointer-events-none flex items-center">
+              <span className="text-default-400 text-small">$</span>
+            </div>
+          }
+          endContent={
+            <div className="flex items-center">
+              <label className="sr-only" htmlFor="currency">
+                Currency
+              </label>
+              <select
+                className="outline-none border-0 bg-transparent text-default-400 text-small"
+                id="currency"
+                name="currency"
+              >
+                <option>USD</option>
+                <option>ARS</option>
+                <option>EUR</option>
+              </select>
+            </div>
+          }
+          type="number"
+        />
+ <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-2">
+        <div className="flex w-full flex-wrap items-end md:flex-nowrap mb-6 md:mb-0 gap-4">
+          {placements.map((placement) => (
+            <DatePicker 
+              label={"Birth date"} 
+              className="max-w-[284px]"
+              description={placement}
+              labelPlacement={"outside"}
+            />
+            
+          ))}
+        </div>
+      </div>  
+    </div> 
+      </div>
+      <Table aria-label="Example static collection table">
+      <TableHeader>
+        <TableColumn>RATING</TableColumn>
+        <TableColumn>CREDIT SCORE</TableColumn>
+        <TableColumn>INCOME</TableColumn>
+      </TableHeader>
+      <TableBody>
+        <TableRow key="1">
+          <TableCell>5.00</TableCell>
+          <TableCell>CEO</TableCell>
+          <TableCell>Active</TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+    </div>  
+    
   );
 }
