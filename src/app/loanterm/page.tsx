6092f96ff6a23405e25ba4586c8ccf871897826c
Hussam Alsaliti <halsaliti@noname-147dda2c010f.dhcp.umsl.edu>
2024-04-27 23:12:19 -0500

    Login and Loan Terms pages initial commit

--- src/app/loanterm/page.tsx
+++ src/app/loanterm/page.tsx
@@ -1,6 +1,62 @@
+'use client'
+
+import React from "react";
+import {User, Link, Textarea, Button, Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, useDisclosure} from "@nextui-org/react";
 
 export default function loanterm() {
+
+  const {isOpen, onOpen, onOpenChange} =useDisclosure();  
   return (
-  <p>loanterm</p>
+    <div>
+    <User
+    name= "Bill Ackman"
+      
+      avatarProps={{
+        src: "https://avatars.githubusercontent.com/u/30373425?v=4"
+      }}
+    />
+
+    <Textarea
+    isReadOnly
+    label="Property information"
+    variant="bordered"
+    labelPlacement="outside"
+    placeholder="Enter your description"
+    defaultValue="Get access to instant cash with this loan"
+    className="max-w-xs" 
+    /> 
+
+<>
+<Button onPress={onOpen} color="primary">ACCEPT</Button>
+<Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+  <ModalContent>
+    {(onClose) => (
+      <>
+        <ModalHeader className="flex flex-col gap-1">Congratulations!</ModalHeader>
+        <ModalBody>
+          <p> 
+           You have successfully accepted this loan. The loan amount will be deposited into your account shortly!
+           </p>
+
+           </ModalBody>
+              <ModalFooter>
+                <Button color="danger" variant="light" onPress={onClose}>
+                  Close
+                </Button>
+                <Link href="/borrowed">
+                <Button color="primary" onClick={onClose}>
+                 My Loans
+                </Button>
+                </Link>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+
+</>
+
+</div>
   );
+ 
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,47 @@
+'use client'
+import React from "react";
+import { FormEvent } from 'react'
+import { useRouter } from 'next/navigation'
+import {Button, Input} from "@nextui-org/react";
 
-export default function Login() {
+
+ 
+export default function LoginPage() {
+  const router = useRouter()
+ 
+  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+ 
+    const formData = new FormData(event.currentTarget)
+    const email = formData.get('email')
+    const password = formData.get('password')
+ 
+    const response = await fetch('/api/auth/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email, password }),
+    })
+ 
+    if (response.ok) {
+      router.push('/profile')
+    } else {
+      // Handle errors
+    }
+  }
+ 
   return (
-  <p>login</p>
-  );
+    <form onSubmit={handleSubmit}>
+      <div
+      className="flex w-full flex-wrap md:flex-nowrap gap-8" >
+      <Input type="email" name="email" placeholder="Email" required />
+      <Input type="password" name="password" placeholder="Password" required />
+      </div>
+      <Button color="primary" size="sm" variant="ghost">Login</Button>
+      
+    </form>
+  )
 }
+
+
+
+
